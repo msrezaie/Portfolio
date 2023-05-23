8ace1871806bfa4ef9da42d12912f1651f7c6920
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2023-05-23 11:07:14 -0400

    initialized boilerplate files
    included front end them from HTML5UP

---
+++ controllers/main-controller.js
@@ -0,0 +1,5 @@
+const getHome = (req, res, next) => {
+  res.render("index");
+};
+
+module.exports = { getHome };
---
+++ index.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const mainRoutes = require("./routes/main");
+
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(mainRoutes);
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server running in port ${PORT}`));
