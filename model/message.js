b3690963e38a6d72555fd8b97046ecf754fa9a95
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2023-08-05 15:06:28 -0400

    updated front page text
    updated resume

--- model/message.js
+++ model/message.js
@@ -7,7 +7,11 @@ const messageSchema = mongoose.Schema({
   },
   email: {
     type: String,
-    required: true,
+    required: [true, "Please provide an email!"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide a valid email!",
+    ],
   },
   messageBody: {
     type: String,
