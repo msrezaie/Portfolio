2c765156274cb37ca461a2dc479c7d27c597950f
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2023-08-15 17:48:16 -0400

    links added/removed

---
+++ model/profile.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const profileSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Profile", profileSchema);
