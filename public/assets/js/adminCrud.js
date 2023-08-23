51d77c57f6cb1e100a2372961e8e105ad012da91
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2023-08-23 17:33:06 -0400

    ejs functionality for the front end
    axios api is used to send the post requests to the back end for updating profile data,

--- public/assets/js/adminCrud.js
+++ public/assets/js/adminCrud.js
@@ -1,10 +1,54 @@
 const logoutBtn = document.getElementById("logout");
+const eraseBtn = document.getElementById("clearEntry");
+const adminForm = document.getElementById("adminForm");
+const resumeFile = document.getElementById("resumeFile");
+const email = document.getElementById("emailLink");
+const phone = document.getElementById("phoneNum");
+const github = document.getElementById("githubLink");
+const linkedin = document.getElementById("linkedinLink");
+
+adminForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const formData = new FormData();
+
+  formData.append("resume", resumeFile.files[0]);
+
+  try {
+    const response = await axios.post("/admin/profile-update", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    Toastify({
+      text: response.data.msg,
+      className: "info",
+      style: {
+        background: "linear-gradient(to right, #A594AB, #A594AB)",
+      },
+    }).showToast();
+  } catch (error) {
+    Toastify({
+      text: error.response.data.msg,
+      className: "error",
+      style: {
+        background: "linear-gradient(to right, #A594AB, #A594AB)",
+      },
+    }).showToast();
+  }
+});
 
 logoutBtn.addEventListener("click", async () => {
   try {
-    await axios.post("/logout");
+    await axios.post("/auth/logout");
     location.reload();
   } catch (error) {
-    console.log(error);
+    Toastify({
+      text: error.response.data.msg,
+      className: "info",
+      style: {
+        background: "linear-gradient(to right, #A594AB, #A594AB)",
+      },
+    }).showToast();
   }
 });
