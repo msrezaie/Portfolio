145e5de159ec42c9220e3130b4211fad4dbc24bb
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2023-08-23 17:34:26 -0400

    profile update endpoints
    profile update functionality implemented,
    data from the models is sent to the views through ejs context

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,42 @@
-const getAdmin = (req, res) => {
-  res.render("admin");
+const fs = require("fs").promises;
+const Profile = require("../models/Profile");
+
+const getAdminPage = async (req, res) => {
+  const profile = await Profile.find();
+  if (profile.length < 1) {
+    res.render("admin", {
+      profile: [
+        {
+          resumePath: "uploads/MSR-Resume.pdf",
+        },
+      ],
+    });
+  } else {
+    res.render("admin", { profile });
+  }
 };
 
-module.exports = { getAdmin };
+const updateProfile = async (req, res) => {
+  if (req.file) {
+    const profile = await Profile.find();
+    if (profile.length < 1) {
+      const newProfile = await Profile.create({ resumePath: req.file.path });
+      res.status(201).json({ msg: "profile entry added", newProfile });
+    } else {
+      const updatedProfile = await Profile.findByIdAndUpdate(
+        {
+          _id: profile[0]._id,
+        },
+        { resumePath: req.file.path },
+        { new: true }
+      );
+      const result = await fs.unlink(profile[0].resumePath);
+      console.log(result);
+      res.status(200).json({ msg: "profile updated", updatedProfile });
+    }
+  } else {
+    res.redirect("/admin");
+  }
+};
+
+module.exports = { getAdminPage, updateProfile };
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,27 +1,25 @@
-const Message = require("../model/message");
+const Message = require("../models/Message");
+const Profile = require("../models/Profile");
 
-const getHome = (req, res, next) => {
-  res.render("index");
+const getHome = async (req, res) => {
+  const profile = await Profile.find();
+  if (profile.length < 1) {
+    res.render("index", {
+      profile: [
+        {
+          resumePath: "uploads/MSR-Resume.pdf",
+        },
+      ],
+    });
+  } else {
+    res.render("index", { profile });
+  }
 };
 
-const postMessage = (req, res, next) => {
-  const name = req.body.name;
-  const email = req.body.email;
-  const messageBody = req.body.messageBody;
-  const message = Message({
-    name: name,
-    email: email,
-    messageBody: messageBody,
-  });
-  message
-    .save()
-    .then(() => {
-      console.log("Message from visitor Saved!");
-      res.redirect("/");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+const postMessage = async (req, res) => {
+  await Message.create(req.body);
+  console.log("Message from visitor Saved!");
+  res.redirect("/");
 };
 
 module.exports = { getHome, postMessage };
